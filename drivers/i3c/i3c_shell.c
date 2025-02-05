a92abccf96417e4e7b438a036c1b6f7bf15e8e10
Alan Yang <tyang1@nuvoton.com>
2025-02-05 13:49:09 +0800

    drivers: i3c: code optimize and add target driver
    The target driver is under development:
    1. Differ pdma and mdma
    2. Check target write/read dma flow
    
    Signed-off-by: Alan Yang <tyang1@nuvoton.com>

--- drivers/i3c/i3c_shell.c
+++ drivers/i3c/i3c_shell.c
@@ -81,6 +81,7 @@ struct i3c_ctrl {
 /* zephyr-keep-sorted-start */
 DT_FOREACH_STATUS_OKAY(cdns_i3c, I3C_CTRL_FN)
 DT_FOREACH_STATUS_OKAY(nuvoton_npcx_i3c, I3C_CTRL_FN)
+DT_FOREACH_STATUS_OKAY(nuvoton_npcm_i3c, I3C_CTRL_FN)
 DT_FOREACH_STATUS_OKAY(nxp_mcux_i3c, I3C_CTRL_FN)
 DT_FOREACH_STATUS_OKAY(st_stm32_i3c, I3C_CTRL_FN)
 /* zephyr-keep-sorted-stop */
@@ -96,6 +97,7 @@ const struct i3c_ctrl i3c_list[] = {
 	/* zephyr-keep-sorted-start */
 	DT_FOREACH_STATUS_OKAY(cdns_i3c, I3C_CTRL_LIST_ENTRY)
 	DT_FOREACH_STATUS_OKAY(nuvoton_npcx_i3c, I3C_CTRL_LIST_ENTRY)
+	DT_FOREACH_STATUS_OKAY(nuvoton_npcm_i3c, I3C_CTRL_LIST_ENTRY)
 	DT_FOREACH_STATUS_OKAY(nxp_mcux_i3c, I3C_CTRL_LIST_ENTRY)
 	DT_FOREACH_STATUS_OKAY(st_stm32_i3c, I3C_CTRL_LIST_ENTRY)
 	/* zephyr-keep-sorted-stop */
